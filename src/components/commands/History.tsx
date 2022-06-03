89f2f7596a9828f26649eb5851762755e73bb471
Sat Naing <satnaingdev@gmail.com>
2022-06-04 01:29:48 +0630

    :bug: update history bug
    change to slice array due to reverse array

--- src/components/commands/History.tsx
+++ src/components/commands/History.tsx
@@ -4,10 +4,10 @@ const History: React.FC<{ index: number; history: string[]; cmd: string }> = ({
   history,
   index,
 }) => {
-  const haha = _.slice(history, 0, index + 1);
+  const currentHistory = _.reverse(_.slice(history, index));
   return (
     <>
-      {haha.map((cmd) => (
+      {currentHistory.map((cmd) => (
         <div key={_.uniqueId(`${cmd}_`)}>{cmd}</div>
       ))}
     </>
