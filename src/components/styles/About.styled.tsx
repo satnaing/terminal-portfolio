807350cb6bdfb3ecf929210a7c38d728bc0b1014
Sat Naing <satnaingdev@gmail.com>
2022-06-04 22:34:41 +0630

    :lipstick: add margin-y for About

--- src/components/styles/About.styled.tsx
+++ src/components/styles/About.styled.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const AboutWrapper = styled.div`
+  margin-top: 0.25rem;
+  margin-bottom: 0.75rem;
   p {
     margin-top: 0.5rem;
     line-height: 1.5rem;
