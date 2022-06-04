6a8c96cadfd6cdbdab0e3ecd0ccfab205e42d102
Sat Naing <satnaingdev@gmail.com>
2022-06-04 21:40:39 +0630

    :lipstick: remove margin-bottom from TerminalInfo

--- src/components/styles/TerminalInfo.styled.tsx
+++ src/components/styles/TerminalInfo.styled.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Wrapper = styled.span`
   display: inline-block;
   margin-right: 0.75rem;
-  margin-bottom: 0.5rem;
 `;
 
 export const WebsiteName = styled.span`
