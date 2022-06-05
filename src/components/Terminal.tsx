d8e5facaccb622dbec1326d75f4eb5e110e6d9ef
Sat Naing <satnaingdev@gmail.com>
2022-06-06 00:58:13 +0630

    :lipstick: update themes tab

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -32,7 +32,7 @@ export const commands: Command = [
   { cmd: "hero-section", desc: "display hero section", tab: 1 },
   { cmd: "socials", desc: "check out my social accounts", tab: 6 },
   { cmd: "pwd", desc: "print current working directory", tab: 10 },
-  { cmd: "themes", desc: "check available themes", tab: 10 },
+  { cmd: "themes", desc: "check available themes", tab: 7 },
 ];
 
 const Terminal = () => {
