688a10a2793fd8a90a51b0fa2ec600b40abeeb5a
Sat Naing <satnaingdev@gmail.com>
2022-06-05 12:39:21 +0630

    :art: add usage div style

--- src/components/styles/Output.styled.tsx
+++ src/components/styles/Output.styled.tsx
@@ -8,3 +8,9 @@ export const Wrapper = styled.div`
   margin-top: 0.25rem;
   margin-bottom: 0.75rem;
 `;
+
+export const UsageDiv = styled.div<{ marginY?: boolean }>`
+  margin-top: ${(props) => (props.marginY ? "0.75rem" : "0.25rem")};
+  margin-bottom: 0.75rem;
+  line-height: 1.5rem;
+`;
