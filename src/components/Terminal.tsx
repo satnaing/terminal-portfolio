18afd861f220d62fac710d68735ce2189fa7e630
Sat Naing <satnaingdev@gmail.com>
2022-06-06 14:09:14 +0630

    :art: update command array order

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -20,20 +20,20 @@ type Command = {
 }[];
 
 export const commands: Command = [
-  { cmd: "whoami", desc: "about current user", tab: 7 },
   { cmd: "about", desc: "about Sat Naing", tab: 8 },
-  { cmd: "projects", desc: "view projects that I've coded", tab: 5 },
+  { cmd: "clear", desc: "clear the terminal", tab: 8 },
+  { cmd: "echo", desc: "print out anything", tab: 9 },
   { cmd: "edu-bg", desc: "my education background", tab: 7 },
   { cmd: "email", desc: "send an email to me", tab: 8 },
-  { cmd: "clear", desc: "clear the terminal", tab: 8 },
-  { cmd: "help", desc: "check available commands", tab: 9 },
-  { cmd: "echo", desc: "print out anything", tab: 9 },
-  { cmd: "history", desc: "view command history", tab: 6 },
-  { cmd: "hero-section", desc: "display hero section", tab: 1 },
-  { cmd: "socials", desc: "check out my social accounts", tab: 6 },
-  { cmd: "pwd", desc: "print current working directory", tab: 10 },
-  { cmd: "themes", desc: "check available themes", tab: 7 },
   { cmd: "gui", desc: "go to my portfolio in GUI", tab: 10 },
+  { cmd: "help", desc: "check available commands", tab: 9 },
+  { cmd: "hero-section", desc: "display hero section", tab: 1 },
+  { cmd: "history", desc: "view command history", tab: 6 },
+  { cmd: "projects", desc: "view projects that I've coded", tab: 5 },
+  { cmd: "pwd", desc: "print current working directory", tab: 10 },
+  { cmd: "socials", desc: "check out my social accounts", tab: 6 },
+  { cmd: "themes", desc: "check available themes", tab: 7 },
+  { cmd: "whoami", desc: "about current user", tab: 7 },
 ];
 
 const Terminal = () => {
