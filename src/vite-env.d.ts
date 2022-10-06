4472f50eb5202371e327955a7a6ba26330157bb2
Sat Naing <satnaingdev@gmail.com>
2022-10-06 23:17:09 +0630

    :label: Add DefaultTheme type for styled-components

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="vite/client" />
+
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    id: string;
+    name: string;
+    colors: {
+      body: string;
+      scrollHandle: string;
+      scrollHandleHover: string;
+      primary: string;
+      secondary: string;
+      text: {
+        100: string;
+        200: string;
+        300: string;
+      };
+    };
+  }
+}
