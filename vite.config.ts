f31fdbda75d0df725f8208d7a63c65da0a3171f6
Sat Naing <satnaingdev@gmail.com>
2022-10-07 00:09:33 +0630

    :wrench: Configure test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
@@ -10,4 +11,7 @@ export default defineConfig({
       registerType: "autoUpdate",
     }),
   ],
+  test: {
+    environment: "happy-dom",
+  },
 });
