920a0b277770090869388b77563f27e184ca8eae
Sat Naing <satnaingdev@gmail.com>
2022-10-14 22:24:44 +0630

    :sparkles: Remove trailing backtick when echo

--- src/components/commands/Echo.tsx
+++ src/components/commands/Echo.tsx
@@ -5,8 +5,13 @@ import { termContext } from "../Terminal";
 
 const Echo: React.FC = () => {
   const { arg } = useContext(termContext);
-  const argStr = _.trim(_.trim(_.join(arg, " "), "'"), '"');
-  return <Wrapper>{argStr}</Wrapper>;
+
+  let outputStr = _.join(arg, " ");
+  outputStr = _.trim(outputStr, "'"); // remove trailing single quotes ''
+  outputStr = _.trim(outputStr, '"'); // remove trailing double quotes ""
+  outputStr = _.trim(outputStr, "`"); // remove trailing backtick ``
+
+  return <Wrapper>{outputStr}</Wrapper>;
 };
 
 export default Echo;
