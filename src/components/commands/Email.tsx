bcfff01bccf9ed914e175858236fba65771dc38d
satnaing <satnaingdev@gmail.com>
2022-10-21 22:44:06 +0630

    :coffin: Remove unnecessary usage check

--- src/components/commands/Email.tsx
+++ src/components/commands/Email.tsx
@@ -1,22 +1,19 @@
 import { useContext } from "react";
 import _ from "lodash";
-import { UsageDiv, Wrapper } from "../styles/Output.styled";
+import { Wrapper } from "../styles/Output.styled";
 import { termContext } from "../Terminal";
 
 const Email: React.FC = () => {
-  const { arg, history, rerender } = useContext(termContext);
+  const { history, rerender } = useContext(termContext);
 
   /* ===== get current command ===== */
   const currentCommand = _.split(history[0], " ");
 
   if (rerender && currentCommand[0] === "email" && currentCommand.length <= 1) {
-    // location.href = "mailto:" + "contact@satnaing.dev";
     window.open("mailto:" + "contact@satnaing.dev", "_self");
   }
 
-  return arg.length > 0 ? (
-    <UsageDiv>Usage: email</UsageDiv>
-  ) : (
+  return (
     <Wrapper>
       <span>contact@satnaing.dev</span>
     </Wrapper>
