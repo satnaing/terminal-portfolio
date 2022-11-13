29cf905773c36033390abbcc36864143d0446923
Álvaro <alvarora12@gmail.com>
2022-11-13 21:06:33 +0100

    :necktie: update autocomplete

--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -134,7 +134,7 @@ export const argTab = (
 
   // 7) if input is 'socials go '
   else if (_.startsWith(inputVal, "socials go ")) {
-    ["1.Github", "2.Dev.to", "3.Facebook", "4.Instagram"].forEach(t => {
+    ["1.Github", "2.LinkedIn", "3.Twitter"].forEach(t => {
       hintsCmds = [...hintsCmds, t];
     });
     return hintsCmds;
@@ -143,10 +143,10 @@ export const argTab = (
   // 8) if input is 'projects go '
   else if (_.startsWith(inputVal, "projects go ")) {
     [
-      "1.Sat Naing's Blog",
-      "2.Haru Fashion",
-      "3.Haru API",
-      "4.Tip Calculator",
+      "1.Vocabulaire",
+      "2.Awards Predictions (WIP)",
+      "3.VSCode4Teaching",
+      "4.AdventJS solutions",
     ].forEach(t => {
       hintsCmds = [...hintsCmds, t];
     });
